9674c9860eb0177c819ae627a21ac4020ae94d45
max_krv <maxkr1203@gmail.com>
2022-09-20 23:59:59 +0300

    add font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,12 @@ export default function Document() {
 		<Html>
 			<Head>
 				<Favicon />
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" />
+				<link
+					href="https://fonts.googleapis.com/css2?family=Jost:wght@400;500&display=swap"
+					rel="stylesheet"
+				/>
 			</Head>
 			<body>
 				<Main />
