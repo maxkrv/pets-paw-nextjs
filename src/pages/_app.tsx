f613a10fa9452f19c38604f159567f7e8a2fe494
max_krv <maxkr1203@gmail.com>
2022-09-26 23:24:32 +0300

    add next-seo package, remove old seo component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
+import SEO from "../../next-seo.config";
+import { DefaultSeo } from "next-seo";
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<DefaultSeo {...SEO} />
+			<Component {...pageProps} />
+		</>
+	);
 }
 
 export default MyApp;
