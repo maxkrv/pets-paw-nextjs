f0dd3e9c09eb3715145384b8d5ba69b51471d24e
max_krv <maxkr1203@gmail.com>
2022-10-03 16:19:13 +0300

    update loader

--- src/components/ui/loader/loader.tsx
+++ src/components/ui/loader/loader.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from "react";
 import classes from "./loader.module.scss";
+import classNames from "classnames";
 
 interface LoaderProps {
 	size?: "small" | "regular";
@@ -7,17 +8,15 @@ interface LoaderProps {
 }
 
 const Loader: FC<LoaderProps> = ({ size = "regular", variant = "primary" }) => {
-	return (
-		<span
-			className={`
-				${classes.loader} 
-				${size === "regular" && classes.regular} 
-				${size === "small" && classes.small}
-				${variant === "primary" && classes.primary}
-				${variant === "white" && classes.white}
-			`}
-		></span>
-	);
+	const loaderClasses = classNames({
+		[classes.loader]: true,
+		[classes.regular]: size === "regular",
+		[classes.small]: size === "small",
+		[classes.primary]: variant === "primary",
+		[classes.white]: variant === "white",
+	});
+
+	return <span className={loaderClasses}></span>;
 };
 
 export default Loader;
