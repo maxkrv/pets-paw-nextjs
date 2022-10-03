cdc2f4f87010299fc65f7014a57046c55688d290
max_krv <maxkr1203@gmail.com>
2022-10-03 16:16:26 +0300

    add theme provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,15 @@ import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import SEO from "../../next-seo.config";
 import { DefaultSeo } from "next-seo";
+import { ThemeProvider } from "next-themes";
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<>
 			<DefaultSeo {...SEO} />
-			<Component {...pageProps} />
+			<ThemeProvider enableSystem attribute="class">
+				<Component {...pageProps} />
+			</ThemeProvider>
 		</>
 	);
 }
