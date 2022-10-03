4052e4dbd5b3a608504639c3c63257e780e38a81
max_krv <maxkr1203@gmail.com>
2022-10-03 16:19:41 +0300

    add button component

---
+++ src/components/ui/button/button.tsx
@@ -0,0 +1,51 @@
+import React, { ComponentProps, ElementType, ReactNode } from "react";
+import Ripples from "react-ripples";
+import classes from "./button.module.scss";
+import Loader from "../loader/loader";
+import classNames from "classnames";
+
+type ButtonOwnProps<E extends ElementType = ElementType> = {
+	children: ReactNode;
+	variant?: "primary" | "primarySoft" | "default" | "gray";
+	isLoading?: boolean;
+	loaderVariant?: "primary" | "white";
+	loaderSize?: "small" | "regular";
+	component?: E;
+};
+
+export type ButtonProps<E extends ElementType> = ButtonOwnProps<E> &
+	Omit<ComponentProps<E>, keyof ButtonOwnProps>;
+
+const defaultElement = "button";
+
+function Button<E extends ElementType = typeof defaultElement>({
+	children,
+	variant = "primary",
+	isLoading,
+	loaderVariant = "white",
+	loaderSize = "small",
+	component,
+	...props
+}: ButtonProps<E>) {
+	const TagName = component || defaultElement;
+	const buttonClasses = classNames({
+		[classes.button]: true,
+		[classes.button__primary]: variant === "primary",
+		[classes.button__primarySoft]: variant === "primarySoft",
+		[classes.button__default]: variant === "default",
+		[classes.button__gray]: variant === "gray",
+	});
+
+	return (
+		<Ripples className={classes.button__ripple}>
+			<TagName className={buttonClasses} {...props}>
+				{isLoading && (
+					<Loader variant={loaderVariant} size={loaderSize} />
+				)}
+				{children}
+			</TagName>
+		</Ripples>
+	);
+}
+
+export default Button;
