1f886176f61348f97ae475337137c32356cf6fdd
max_krv <maxkr1203@gmail.com>
2022-10-06 19:57:40 +0300

    add getActive util

---
+++ src/utils/getActive.ts
@@ -0,0 +1,12 @@
+import { useRouter } from "next/router";
+
+const getActive = (classToAdd: string, link: string) => {
+	const router = useRouter();
+	const currentRoute = router.pathname;
+
+	if (currentRoute === link) {
+		return `${classToAdd}`;
+	}
+};
+
+export default getActive;
