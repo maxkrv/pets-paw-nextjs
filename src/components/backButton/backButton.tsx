de724aac08ba1e210303420561e5c0ebd3c31608
max_krv <maxkr1203@gmail.com>
2022-10-14 21:38:03 +0300

    add backButton component

---
+++ src/components/backButton/backButton.tsx
@@ -0,0 +1,32 @@
+import React, { ComponentPropsWithoutRef, FC } from "react";
+import Button from "../ui/button/button";
+import classNames from "classnames";
+import { LeftArrow } from "../../assets/svg";
+import { useRouter } from "next/router";
+
+interface BackButtonProps extends ComponentPropsWithoutRef<"button"> {}
+
+const BackButton: FC<BackButtonProps> = ({ ...props }) => {
+	const router = useRouter();
+	const backButtonClasses = classNames({
+		[props.className as string]: true,
+	});
+	delete props.className;
+
+	return (
+		<>
+			{router.pathname !== "/" && (
+				<Button
+					className={backButtonClasses}
+					variant="primarySoft"
+					onClick={() => router.back()}
+					{...props}
+				>
+					<LeftArrow />
+				</Button>
+			)}
+		</>
+	);
+};
+
+export default BackButton;
