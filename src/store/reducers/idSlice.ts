9e67b8406ede4a87c1060ddc1221d0771aa8ffd1
max_krv <maxkr1203@gmail.com>
2022-11-03 12:56:54 +0200

    add idSlice

---
+++ src/store/reducers/idSlice.ts
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { uuid } from "uuidv4";
+
+interface idSliceState {
+	id: string | null;
+}
+
+const initialState: idSliceState = {
+	id: typeof window !== "undefined" ? localStorage.getItem("id") : null,
+};
+
+const idSlice = createSlice({
+	name: "id",
+	initialState,
+	reducers: {
+		generateId: (state) => {
+			if (state.id === null) {
+				state.id = uuid();
+				localStorage.setItem("id", state.id);
+			}
+		},
+	},
+});
+
+export const { generateId } = idSlice.actions;
+export default idSlice.reducer;
