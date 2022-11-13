b20feeca13b9913bc5c154ae76588e0b604fb2b5
max_krv <maxkr1203@gmail.com>
2022-11-13 23:36:26 +0200

    add userLogSlice

---
+++ src/store/reducers/userLogSlice.ts
@@ -0,0 +1,33 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { ILog } from "../../types/log";
+
+interface userLogState {
+	logs: ILog[];
+}
+
+const initialState: userLogState = {
+	logs: [],
+};
+
+const userLogSlice = createSlice({
+	name: "userLog",
+	initialState,
+	reducers: {
+		setLog: (state, action: PayloadAction<ILog>) => {
+			const log: ILog = {
+				createdAt: action.payload.createdAt,
+				imageId: action.payload.imageId,
+				message: action.payload.message,
+				value: action.payload.value,
+			};
+
+			if (state.logs.length > 4) {
+				state.logs.shift();
+			}
+			state.logs.push(log);
+		},
+	},
+});
+
+export const { setLog } = userLogSlice.actions;
+export default userLogSlice.reducer;
