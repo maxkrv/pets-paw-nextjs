b9588998f3e65c3c86476809e2f4593c230e61db
max_krv <maxkr1203@gmail.com>
2022-11-14 17:50:34 +0200

    add types

--- src/components/ui/loader/loader.tsx
+++ src/components/ui/loader/loader.tsx
@@ -1,22 +1,33 @@
-import React, { FC } from "react";
+import React, { ComponentPropsWithoutRef, FC } from "react";
 import classes from "./loader.module.scss";
 import classNames from "classnames";
 
-interface LoaderProps {
+interface LoaderProps extends ComponentPropsWithoutRef<"span"> {
 	size?: "small" | "regular";
 	variant?: "primary" | "white";
 }
 
-const Loader: FC<LoaderProps> = ({ size = "regular", variant = "primary" }) => {
+const Loader: FC<LoaderProps> = ({
+	size = "regular",
+	variant = "primary",
+	...props
+}) => {
 	const loaderClasses = classNames({
 		[classes.loader]: true,
 		[classes.regular]: size === "regular",
 		[classes.small]: size === "small",
 		[classes.primary]: variant === "primary",
 		[classes.white]: variant === "white",
+		[props.className as string]: true,
 	});
+	delete props.className;
 
-	return <span className={loaderClasses}></span>;
+	return (
+		<span className={loaderClasses} {...props}>
+			{" "}
+			<div className="sr-only">...Loading</div>
+		</span>
+	);
 };
 
 export default Loader;
