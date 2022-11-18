4f46f96974d08af55839d4a8e8b16d792d2b6430
max_krv <maxkr1203@gmail.com>
2022-11-18 13:57:07 +0200

    add centered prop

--- src/components/ui/loader/loader.tsx
+++ src/components/ui/loader/loader.tsx
@@ -5,11 +5,13 @@ import classNames from "classnames";
 interface LoaderProps extends ComponentPropsWithoutRef<"span"> {
 	size?: "small" | "regular";
 	variant?: "primary" | "white";
+	centered?: boolean;
 }
 
 const Loader: FC<LoaderProps> = ({
 	size = "regular",
 	variant = "primary",
+	centered = false,
 	...props
 }) => {
 	const loaderClasses = classNames({
@@ -18,13 +20,13 @@ const Loader: FC<LoaderProps> = ({
 		[classes.small]: size === "small",
 		[classes.primary]: variant === "primary",
 		[classes.white]: variant === "white",
+		["absolute top-2/4 left-2/4"]: centered,
 		[props.className as string]: true,
 	});
 	delete props.className;
 
 	return (
 		<span className={loaderClasses} {...props}>
-			{" "}
 			<div className="sr-only">...Loading</div>
 		</span>
 	);
