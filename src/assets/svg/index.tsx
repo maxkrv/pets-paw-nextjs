54d2af3f868c78a1785dc20c6addfed3331ba6d8
max_krv <maxkr1203@gmail.com>
2022-11-23 00:33:17 +0200

    add burger icon

--- src/assets/svg/index.tsx
+++ src/assets/svg/index.tsx
@@ -124,3 +124,25 @@ export const HeartFilled: FC<ComponentPropsWithoutRef<"svg">> = ({
 		</svg>
 	);
 };
+
+export const BurgerMenu: FC<ComponentPropsWithoutRef<"svg">> = ({
+	...props
+}) => {
+	return (
+		<svg
+			width="30"
+			height="18"
+			viewBox="0 0 30 18"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+			{...props}
+		>
+			<path
+				fillRule="evenodd"
+				clipRule="evenodd"
+				d="M30 2H0V0H30V2ZM30 10H0V8H30V10ZM30 18H0V16H30V18Z"
+				fill="#FF868E"
+			/>
+		</svg>
+	);
+};
