503ae14f2154944ab47527a729d079fe2854a4e9
max_krv <maxkr1203@gmail.com>
2022-11-23 13:19:31 +0200

    add types

--- src/components/darkModeSwitcher/darkModeSwitcher.tsx
+++ src/components/darkModeSwitcher/darkModeSwitcher.tsx
@@ -1,9 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React, {
+	ComponentPropsWithoutRef,
+	FC,
+	useEffect,
+	useState,
+} from "react";
 import { Moon } from "../../assets/svg";
 import Toggle from "../ui/toggle/toggle";
 import { useTheme } from "next-themes";
+import classNames from "classnames";
 
-const DarkModeSwitcher = () => {
+const DarkModeSwitcher: FC<ComponentPropsWithoutRef<"div">> = ({
+	...props
+}) => {
 	const [enabled, setEnabled] = useState<boolean>(false);
 	const { theme, setTheme } = useTheme();
 
@@ -19,8 +27,14 @@ const DarkModeSwitcher = () => {
 		}
 	}, [theme, enabled]);
 
+	const darkModeSwitcherClasses = classNames({
+		["flex items-center gap-2"]: true,
+		[props.className as string]: true,
+	});
+	delete props.className;
+
 	return (
-		<div className="flex items-center gap-2">
+		<div className={darkModeSwitcherClasses}>
 			<div className="h-6 w-6 flex items-center justify-center rounded-full bg-white">
 				<Moon />
 			</div>
