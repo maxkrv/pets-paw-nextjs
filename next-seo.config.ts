cf4f7461ceda94258352fdc97c9fbd1d43d772c7
max_krv <maxkr1203@gmail.com>
2022-12-14 11:49:00 +0200

    update next-seo.config.ts

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -6,9 +6,21 @@ export default {
 		locale: "en_IE",
 		url:
 			process.env.NODE_ENV === "production"
-				? "http://localhost:3000/"
-				: process.env.SITE_URL,
+				? process.env.SITE_URL
+				: "http://localhost:3000",
 		site_name: "PetsPaw",
+		images: [
+			{
+				url: `${
+					process.env.NODE_ENV === "production"
+						? process.env.SITE_URL
+						: "http://localhost:3000"
+				}/favicon/android-chrome-192x192.png`,
+				width: 192,
+				height: 192,
+				alt: "Og Image Alt",
+			},
+		],
 	},
 	twitter: {
 		handle: "@leenp1ck",
